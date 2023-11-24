04262f983852800d990d842f7c61e62acce40c88
Piper <119914567+pipkickflip@users.noreply.github.com>
2023-11-24 16:11:58 -0700

    Update mode-switcher.js

--- js/mode-switcher.js
+++ js/mode-switcher.js
@@ -32,12 +32,12 @@ function modeSwitcher() {
 		sessionStorage.setItem('theme', 'dark');
 		//document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	} else if (systemInitiatedDark.matches) {	
-		document.documentElement.setAttribute('data-theme', 'dark');
-		sessionStorage.setItem('theme', 'dark');
-		//document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-	} else {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
+		//document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
 		//document.getElementById("theme-toggle").innerHTML = "Light Mode";
 	}
 }
